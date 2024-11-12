6b285fce249a5eaf62bfec1441bd1e8549182731
Sachin Sekiro <chriswilderdelta19@gmail.com>
2024-11-12 23:29:44 +0530

    Update logic2.js

--- colorChanger/logic2.js
+++ colorChanger/logic2.js
@@ -1,8 +1,6 @@
 // In this script we select all button at once, So that we can 
 // apply forloops on them
 
-// Change src in script of index to logic2.js
-
     // Remember to use queryselectorAll dont ommit All
 const buttons = document.querySelectorAll(".button")
 const body = document.querySelector('body')
@@ -11,9 +9,26 @@ const body = document.querySelector('body')
 
 buttons.forEach( (button) =>
 {
-    button.addEventListener("click", ()=>{       
-        body.style.backgroundColor = button.id; // This callback can access
+    button.addEventListener("click", ( e)=>{       
+
+        // Note that param taken by callback of event function
+        // is called event object
+        console.log(e); 
+        // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+        // Hence e is a object representing pointer attribute  details about the click event, 
+        // such as pointer position, whether the event is trusted etc
+        
+        // target is a property of even object carrying info regarding
+        // element on which this even will be applied.
+        console.log(e.target);
+        //<span class="button" id="white"></span>
+
+        // Since we clicked the white button, it is the current target now
+
+        body.style.backgroundColor = button.id; 
+                    // This callback can access
                     // both button variable and body variable since
                     // They are in its lexical scope
     })
 })
+
